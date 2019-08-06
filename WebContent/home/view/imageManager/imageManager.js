df64d4d1a2ee21a8d5da628e845d97acc853d60d
wuchen9999 <453821963@qq.com>
2019-08-06 16:24:28 +0800

    ++

--- WebContent/home/view/imageManager/imageManager.js
+++ WebContent/home/view/imageManager/imageManager.js
@@ -1,12 +1,48 @@
-
-var fileVM =  new Vue({
-    el:'#fileVM',
-    data : {data:""},
-    beforeCreate:function(){
-        var url="http://localhost:8090/getFilesInfo";
-        var _self=this;
-        $.post("http://localhost:8090/getFilesInfo",function(data){
-            _self.data=eval("(" + data +")");
-        })
+new Vue({
+    el: '#fileVM',
+    data: {
+        fileList: [],
+        file: {
+            id: '',
+            empName: '',
+            email: '',
+            gender: '',
+            birth: '',
+            dept_id: ''
+        }
+    },
+    created: function () {
+        var _this = this;
+        axios.post('http://localhost:8090/getFilesInfo')
+            .then(function (res) {
+                console.log(res);
+                _this.fileList = res.data;
+            })
+            .catch(function (error) {
+                alert(error)
+            })
+    },
+    methods: {
+        delFile: function (id) {
+            // var _this=this;
+            axios.get('/delFile/' + id)
+                .then(function () {
+                    alert("删除成功");
+                    window.location.reload();
+                })
+                .catch(function (error) {
+                    alert(error)
+                })
+        },
+        queryByFileId:function (id) {
+            var _this = this;
+            axios.get('/queryByFileId/'+id)
+                .then(function(res) {
+                    emp.emp = res.data;
+                })
+                .catch(function(error) {
+                    alert(error)
+                })
+        }
     }
-});
+})
