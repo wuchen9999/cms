27ba8c1d98d54f8b8a9af1d3025248f6dbbbb431
wuchen9999 <453821963@qq.com>
2019-08-06 09:02:58 +0800

    ++

--- WebContent/home/view/imageManager/imageManager.js
+++ WebContent/home/view/imageManager/imageManager.js
@@ -1,83 +1,12 @@
+
 var fileVM =  new Vue({
     el:'#fileVM',
-    data : function () {
-        ajax
+    data : {data:""},
+    beforeCreate:function(){
+        var url="http://localhost:8090/getFilesInfo";
+        var _self=this;
+        $.post("http://localhost:8090/getFilesInfo",function(data){
+            _self.data=eval("(" + data +")");
+        })
     }
 });
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
-});
-
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: '页面加载于 ' + new Date().toLocaleString()
-    }
-})
-
-var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        seen: false
-    }
-})
-
-var app4 = new Vue({
-    el: '#app-4',
-    data: {
-        todos: [
-            { text: '学习 JavaScript' },
-            { text: '学习 Vue' },
-            { text: '整个牛项目' }
-        ]
-    }
-})
-var app5 = new Vue({
-    el: '#app-5',
-    data: {
-        message: 'Hello Vue.js!'
-    },
-    methods: {
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('')
-        }
-    }
-})
-
-var app6 = new Vue({
-    el: '#app-6',
-    data: {
-        message: 'Hello heeeee!'
-    }
-})
-
-// 定义名为 todo-item 的新组件
-// Vue.component('todo-item', {
-//     template: '<li>这是个待办项</li>'
-// })
-
-// Vue.component('todo-item', {
-//     // todo-item 组件现在接受一个
-//     // "prop"，类似于一个自定义特性。
-//     // 这个 prop 名为 todo。
-//     props: ['todo'],
-//     template: '<li>{{ todo.text }}</li>'
-// })
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-})
-
-var app7 = new Vue({
-    el: '#app-7',
-    data: {
-        groceryList: [
-            { id: 0, text: '蔬菜' },
-            { id: 1, text: '奶酪' },
-            { id: 2, text: '随便其它什么人吃的东西' }
-        ]
-    }
-})
